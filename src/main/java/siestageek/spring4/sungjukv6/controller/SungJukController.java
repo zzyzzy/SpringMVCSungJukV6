4c9047ebcb80f179f48a9f4d093388a670913dd8
zzyzigy <zzyzigy@gmail.com>
2023-04-25 16:08:22 +0900

    스프링MVC 프로젝트 - 성적 조회 3

--- src/main/java/siestageek/spring4/sungjukv6/controller/SungJukController.java
+++ src/main/java/siestageek/spring4/sungjukv6/controller/SungJukController.java
@@ -3,6 +3,7 @@ package siestageek.spring4.sungjukv6.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,13 +19,15 @@ public class SungJukController {
         this.sjsrv = sjsrv;
     }
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String list( Model model ) {
-        System.out.println(sjsrv.readSungJuk());
+    @GetMapping(value = "/list")
+    public ModelAndView list(  ) {
+        ModelAndView mv = new ModelAndView();
 
         // sungjuklist.jsp에 성적조회결과를 sjs라는 이름으로 넘김
-        model.addAttribute("sjs", sjsrv.readSungJuk());
-        return "sungjuklist";
+        mv.addObject("sjs", sjsrv.readSungJuk());
+        mv.setViewName("sungjuklist");  // 뷰 이름 지정
+
+        return mv;
     }
 
 }
