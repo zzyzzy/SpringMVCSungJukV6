8743d3f3e8a50cc420d04b84449301d0017b31e9
zzyzigy <zzyzigy@gmail.com>
2023-04-28 16:21:10 +0900

    mybatis3 설정

--- src/main/java/siestageek/spring4/sungjukv6/dao/SungJukV6DAOImpl.java
+++ src/main/java/siestageek/spring4/sungjukv6/dao/SungJukV6DAOImpl.java
@@ -1,11 +1,9 @@
 package siestageek.spring4.sungjukv6.dao;
 
+import org.apache.ibatis.session.SqlSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import siestageek.spring4.sungjukv6.model.SungJukVO;
 
@@ -17,97 +15,37 @@ import java.util.List;
 public class SungJukV6DAOImpl implements SungJukV4DAO {
 
     private static final Logger logger = LogManager.getLogger(SungJukV6DAOImpl.class);
-    // debug, info(!), warn, error(!), fatal
-    private JdbcTemplate jdbcTemplate;
 
-    // sql.properties 에 정의한 SQL 가져오기
-    @Value("#{sql['insertSQL']}") private String insertSQL;
-    @Value("#{sql['selectSQL']}") private String selectSQL;
-    @Value("#{sql['selectOneSQL']}") private String selectOneSQL;
-    @Value("#{sql['updateSQL']}") private String updateSQL;
-    @Value("#{sql['deleteSQL']}") private String deleteSQL;
+    private SqlSession sqlSession;
 
     @Autowired
-    public SungJukV6DAOImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public SungJukV6DAOImpl(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
     }
 
     @Override
     public int insertSungJuk(SungJukVO sj) {
-        int cnt = -1;
-
-        try {
-            // 매개변수 정의
-            Object[] params = new Object[]{
-                    sj.getName(), sj.getKor(), sj.getEng(),
-                    sj.getMat(), sj.getTot(), sj.getAvg(), sj.getGrd()+""
-            };
-            cnt = jdbcTemplate.update(insertSQL, params);
-        } catch (Exception ex) {
-            logger.error("insertSungJuk 오류!!");
-            logger.info(ex.getMessage());
-        }
-
-        return cnt;
+        return 0;
     }
 
     @Override
     public List<SungJukVO> selectSungJuk() {
-        // 콜백 메서드 선언
-        // 즉, SQL을 실행하고, 결과집합이 존재하는 경우
-        // 스프링컨테이너에 의해 자동으로 호출하는 메서드
-        RowMapper<SungJukVO> mapper = new SungJukMapper();
-
-        return jdbcTemplate.query(selectSQL, mapper);
-    }
-
-    private class SungJukMapper implements RowMapper<SungJukVO> {
-        @Override
-        public SungJukVO mapRow(ResultSet rs, int num) throws SQLException {
-            SungJukVO sj = new SungJukVO(rs.getString(2),
-                    rs.getInt(3),rs.getInt(4),rs.getInt(5));
-            sj.setSjno(rs.getInt(1));
-
-            return sj;
-        }
+        return null;
     }
 
     @Override
     public SungJukVO selectOneSungJuk(int sjno) {
-        Object[] param = new Object[] { sjno };
-        RowMapper<SungJukVO> mapper = new SungJukOneMapper();
-        SungJukVO sj =
-            jdbcTemplate.queryForObject(selectOneSQL, mapper, param);
-
-        return sj;
-    }
-
-    private class SungJukOneMapper implements RowMapper<SungJukVO> {
-        @Override
-        public SungJukVO mapRow(ResultSet rs, int num) throws SQLException {
-            SungJukVO sj = new SungJukVO(rs.getString(2),
-                rs.getInt(3),rs.getInt(4),rs.getInt(5),rs.getInt(6),
-                    rs.getDouble(7), rs.getString(8).charAt(0));
-            sj.setSjno(rs.getInt(1));
-            sj.setRegdate(rs.getString(9));
-
-            return sj;
-        }
+        return null;
     }
 
     @Override
     public int updateSungJuk(SungJukVO sj) {
-        Object[] param = new Object[] { sj.getKor(), sj.getEng(), sj.getMat(),
-            sj.getTot(), sj.getAvg(), sj.getGrd()+"", sj.getSjno() };
-
-        return jdbcTemplate.update(updateSQL, param);
+        return 0;
     }
 
     @Override
     public int deleteSungJuk(int sjno) {
-        Object[] param = new Object[] { sjno };
-
-        return jdbcTemplate.update(deleteSQL, param);
+        return 0;
     }
 
 
